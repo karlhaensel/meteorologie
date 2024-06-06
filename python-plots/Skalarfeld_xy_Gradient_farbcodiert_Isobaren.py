87b20563ad801321dc337354f0ce421d1d9989cd
Karl Hänsel <Karl-Haensel@gmx.de>
2024-06-06 16:24:12 +0200

    Added standard use of plt.subplots() to Skalarfeld_xy_Gradient_farbcordiert_Isobaren

--- python-plots/Skalarfeld_xy_Gradient_farbcodiert_Isobaren.py
+++ python-plots/Skalarfeld_xy_Gradient_farbcodiert_Isobaren.py
@@ -1,8 +1,8 @@
 ﻿# Skript zum Plotten eines 2D-Gradienten samt Isolinien
 
 # Beispiel-Feld hier (fuer z=0): p(x,y) = 1000+(0.1x+0.5)(-0.2y+1.0)
-# fuer ein anderes Feld p durch Felddefinition ersetzen und in
-# gradient(x,y) xgrad durch x-Komponente des Gradienten ersetzen (genauso y)
+# fuer ein anderes Feld p ggf. umbenennen durch Felddefinition ersetzen und in
+# gradient(x,y) xgrad durch x-Komponente des Gradienten ersetzen (ebenso ygrad)
 
 
 # benoetigte Bibliothek importieren
@@ -32,22 +32,24 @@ def gradient(x, y):
 XGRAD, YGRAD = gradient(X, Y)
 betraege = np.sqrt(XGRAD**2 + YGRAD**2)
 
+# Plot vorbereiten
+fig, ax = plt.subplots()
 
 # Gradient-Vektoren zeichnen und nach jeweiligem Betrag faerben
-plt.quiver(X, Y, XGRAD, YGRAD, betraege, cmap='coolwarm')
+ax.quiver(X, Y, XGRAD, YGRAD, betraege, cmap='coolwarm')
 
 # Isobaren einzeichnen (Variable levels bestimmt Anzahl der Isobaren)
-plt.contour(X, Y, p, levels=10, colors='g')
+ax.contour(X, Y, p, levels=10, colors='g')
 
 # Beschriftungen
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('Gradient mit Isobaren (z=0)')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_title('Gradient mit Isobaren (z=0)')
 
 # Farb-Legende fuer Gradient-Betraege
 farbskala = cm.ScalarMappable(cmap='coolwarm')
 farbskala.set_array(betraege)
-legende = plt.colorbar(farbskala)
+legende = plt.colorbar(farbskala, ax=ax)
 legende.set_label('Betrag des Gradienten')
 
 plt.show()
