0a6198666abaaf47e1077335f19e92ca72a03525
Karl Hänsel <Karl-Haensel@gmx.de>
2024-06-06 16:08:19 +0200

    Improved comments on plot for 2D trajectory

--- python-plots/Trajektorie_2D.py
+++ python-plots/Trajektorie_2D.py
@@ -1,12 +1,14 @@
 ﻿# Skript zum Plotten einer 2D-Trajektorie
+# hier als Beispiel der 2D-Ortsvektor r(t) mit  x = 2t + sin(2t); y = 3t
 
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Trajektorie definieren
+# Zeit t als unabhängige Variable definieren
 t = np.linspace(0, 10, 100)
 
 
+# Trajektorie in Abhaengigkeit von t definieren
 def x(t):
     return 2*t + np.sin(2*t)
 
