fcda31963b19d83a2e53d4a561140b25cdbcbc1e
Karl Hänsel <Karl-Haensel@gmx.de>
2024-06-06 16:50:05 +0200

    Added use of plt.subplots() to Skalarfeld_xt_farbcodiert_Punktmarkierung

--- python-plots/Skalarfeld_xt_farbcodiert_Punktmarkierung.py
+++ python-plots/Skalarfeld_xt_farbcodiert_Punktmarkierung.py
@@ -1,4 +1,10 @@
-﻿# benoetigte Bibliothek importieren
+﻿# Skript zum Plotten eines Skalarfeldes samt zeitlicher Aenderung 
+# mit Farbcodierung und Punktmarkierung
+
+# Beispiel-Feld hier (fuer z=0): p(x,y) = 1000+(0.1x+0.5)(-0.2y+1.0)
+# fuer ein anderes Feld p durch Felddefinition ersetzen und ggf. umbenennen
+
+# benoetigte Bibliothek importieren
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -10,22 +16,25 @@ x, t = np.meshgrid(X, Y)
 # Definition Feld T
 T = np.exp(-25*(x-t)**2)
 
+# Plot vorbereiten
+fig, ax = plt.subplots()
+
 # Feld plotten und nach Hoehe des Wertes faerben
-plt.contourf(x, t, T, cmap='coolwarm', levels=np.linspace(0, 1, 11))
-plt.colorbar(label='Temperatur T(x,t)')
+contour = ax.contourf(x, t, T, cmap='coolwarm', levels=np.linspace(0, 1, 11))
+fig.colorbar(contour, ax=ax, label='Temperatur T(x,t)')  # ergaenzt Farblegende
 
 # Punkt markieren und beschriften
-plt.plot(
+ax.plot(
     0.6, 0.75, 'x',
     markerfacecolor='black', markeredgecolor='black', markersize=7)
-plt.text(
+ax.text(
     0.6, 0.79, '(0.6, 0.75)',
     color='black', fontsize=10, ha='center', va='center')
 
 # Beschriftungen
-plt.xlabel('Weg x')
-plt.ylabel('Zeit t')
-plt.title('Temperaturverlauf im x-t-Diagramm')
+ax.set_xlabel('Weg x')
+ax.set_ylabel('Zeit t')
+ax.set_title('Temperaturverlauf im x-t-Diagramm')
 
 # Grafik ggf. speichern und anzeigen
 # plt.savefig('feld.png')
