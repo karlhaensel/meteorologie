cd48592e2d0e298e4c40138ed1a1baf4190d6298
Karl Hänsel <Karl-Haensel@gmx.de>
2025-05-12 14:11:01 +0200

    Add python-scripts\windprofil.py

---
+++ python-scripts/windprofile.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+def windprofil_hellmann(v_1: float, h_1: float, h_2: float, alpha: float = 0.4) -> float:
+    """
+    Berechnung Windgeschwindigkeit v_2 auf Höhe h_2 nach Hellmann-Formel
+    unter Nutzung von gemessener Windgeschwindigkeit v_1 auf Höhe h_1
+    und gemäß Höhenkoeffizient alpha (abgeleitet von Rauigkeitshöhe z_0)
+    typische Werte für alpha:
+    Hohe See 0.1
+    Flaches Land 0.16
+    Wald 0.28
+    Städtische Bebauung 0.4
+    """
+    return v_1 * (h_2/h_1)**alpha
+
+
+def windprofil_logarithmisch(v_r: float, h_r: float, h: float, z_0: float) -> float:
+    """
+    Berechnung Windgeschwindigkeit v auf Höhe h nach logarithmischen Windprofil (Prandtl)
+    unter Nutzung von gemessener Referenzgeschwindigkeit v_r auf Referenzhöhe h_r
+    sowie Rauigkeitslänge z_0
+    typische Werte für z_0:
+    Offshore (v_10 = 5 m/s) 0.0001
+    Vorstädte 0.5
+    Wald 0.8
+    Größere Städte mit hohen Gebäuden 1.0
+    Großstädte mit hohen Gebäuden und Wolkenkratzern 1.6
+    """
+    return v_r * np.log(h/z_0)/np.log(h_r/z_0)
+
+
+if __name__ == '__main__':
+    # Beispiel Hamburg (z_0 = 0.9 -> alpha = 0.34), Messung 4.4 m/s in h = 10 m
+    Z_0 = 0.9
+    ALPHA = 0.33
+    for h in (50, 120, 180, 220, 250, 280):
+        print(
+            f'{h:3} m: {windprofil_logarithmisch(4.4, 10, h, Z_0):6.2f} m/s (logarithmisch), '
+            f'{windprofil_hellmann(4.4, 10, h, ALPHA):6.2f} m/s (Hellmann)'
+        )
+    # Beispiel Stuttgart Umland (z_0 = 0.7), Messung 2.6 m/s in h = 7 m
+    print(f'80 m Nabenhöhe: {windprofil_logarithmisch(2.6, 7, 80, 0.7):.2f} m/s')
